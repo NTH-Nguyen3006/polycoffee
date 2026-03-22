244c02d8dd44ddc6154db0caf4709fd4f4d8a71b
Nguyễn Nam <soniclolgaming@gmail.com>
2026-03-22 22:02:59 +0700

    Feat: Add standard DAO pattern implementations using XJPA

---
+++ src/main/java/com/polycoffee/dao/impl/OrderItemDAOImpl.java
@@ -0,0 +1,78 @@
+package com.polycoffee.dao.impl;
+
+import com.polycoffee.dao.OrderItemDAO;
+import com.polycoffee.entity.OrderItem;
+import com.polycoffee.utils.XJPA;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+
+public class OrderItemDAOImpl implements OrderItemDAO {
+
+    @Override
+    public void insert(OrderItem entity) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void update(OrderItem entity) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(entity);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            OrderItem entity = em.find(OrderItem.class, id);
+            if (entity != null) {
+                em.remove(entity);
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public OrderItem findById(Long id) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            return em.find(OrderItem.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<OrderItem> findAll() {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            return em.createQuery("SELECT o FROM OrderItem o", OrderItem.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+}
