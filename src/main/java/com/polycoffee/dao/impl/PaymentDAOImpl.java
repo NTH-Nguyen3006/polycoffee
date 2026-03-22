19f877e8edf178571c8d0225268330fe9ae0daf9
Nguyễn Nam <soniclolgaming@gmail.com>
2026-03-22 22:02:59 +0700

    Feat: Add standard DAO pattern implementations using XJPA

---
+++ src/main/java/com/polycoffee/dao/impl/PaymentDAOImpl.java
@@ -0,0 +1,78 @@
+package com.polycoffee.dao.impl;
+
+import com.polycoffee.dao.PaymentDAO;
+import com.polycoffee.entity.Payment;
+import com.polycoffee.utils.XJPA;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+
+public class PaymentDAOImpl implements PaymentDAO {
+
+    @Override
+    public void insert(Payment entity) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void update(Payment entity) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(entity);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            Payment entity = em.find(Payment.class, id);
+            if (entity != null) {
+                em.remove(entity);
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Payment findById(Long id) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            return em.find(Payment.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Payment> findAll() {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            return em.createQuery("SELECT p FROM Payment p", Payment.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+}
---
+++ src/main/java/com/polycoffee/dao/impl/PromotionDAOImpl.java
@@ -0,0 +1,78 @@
+package com.polycoffee.dao.impl;
+
+import com.polycoffee.dao.PromotionDAO;
+import com.polycoffee.entity.Promotion;
+import com.polycoffee.utils.XJPA;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+
+public class PromotionDAOImpl implements PromotionDAO {
+
+    @Override
+    public void insert(Promotion entity) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void update(Promotion entity) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(entity);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            Promotion entity = em.find(Promotion.class, id);
+            if (entity != null) {
+                em.remove(entity);
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Promotion findById(Long id) {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            return em.find(Promotion.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Promotion> findAll() {
+        EntityManager em = XJPA.getEntityManager();
+        try {
+            return em.createQuery("SELECT p FROM Promotion p", Promotion.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+}
