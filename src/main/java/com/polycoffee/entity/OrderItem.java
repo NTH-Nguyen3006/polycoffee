a79584f7fe70c0a61945a94f64e4edabeaf0affa
NTH-Nguyen <nthn300607@gmail.com>
2026-03-22 23:25:19 +0700

    fix conflict from nam branch

--- src/main/java/com/polycoffee/entity/OrderItem.java
+++ src/main/java/com/polycoffee/entity/OrderItem.java
@@ -1,9 +1,15 @@
 package com.polycoffee.entity;
 
-import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "ORDER_ITEMS")
