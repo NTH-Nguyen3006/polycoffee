d40fe5af32805ee052dacecd85a1802f50c9c1b7
Nguyễn Nam <soniclolgaming@gmail.com>
2026-03-22 22:23:22 +0700

    Refactor: Implement Generic CRUD interface for DAOs

--- src/main/java/com/polycoffee/dao/OrderItemDAO.java
+++ src/main/java/com/polycoffee/dao/OrderItemDAO.java
@@ -1,12 +1,6 @@
 package com.polycoffee.dao;
 
 import com.polycoffee.entity.OrderItem;
-import java.util.List;
 
-public interface OrderItemDAO {
-    void insert(OrderItem entity);
-    void update(OrderItem entity);
-    void delete(Long id);
-    OrderItem findById(Long id);
-    List<OrderItem> findAll();
+public interface OrderItemDAO extends CRUD<Long, OrderItem> {
 }
