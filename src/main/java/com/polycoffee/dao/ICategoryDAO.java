cbc764b45753414639cd6d48b43b450938a960e4
NTH-Nguyen <nthn300607@gmail.com>
2026-03-22 23:06:40 +0700

    feat: Implement data access objects for Users and Categories, introducing the Users entity and UserRole enum.

---
+++ src/main/java/com/polycoffee/dao/ICategoryDAO.java
@@ -0,0 +1,7 @@
+package com.polycoffee.dao;
+
+import com.polycoffee.entity.Categories;
+
+public interface ICategoryDAO extends CRUD<Integer, Categories> {
+
+}
---
+++ src/main/java/com/polycoffee/dao/IUserDAO.java
@@ -0,0 +1,7 @@
+package com.polycoffee.dao;
+
+import com.polycoffee.entity.Users;
+
+public interface IUserDAO extends CRUD<String, Users> {
+
+}
