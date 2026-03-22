f66caf0e079b56dfff873e8557941f1f56014058
NTH-Nguyen <nthn300607@gmail.com>
2026-03-22 21:39:25 +0700

    create project

--- src/main/java/com/polycoffee/dao/OrderItemDAO.java
+++
@@ -1,6 +0,0 @@
-package com.polycoffee.dao;
-
-import com.polycoffee.entity.OrderItem;
-
-public interface OrderItemDAO extends CRUD<Long, OrderItem> {
-}
--- src/main/java/com/polycoffee/dao/PaymentDAO.java
+++
@@ -1,6 +0,0 @@
-package com.polycoffee.dao;
-
-import com.polycoffee.entity.Payment;
-
-public interface PaymentDAO extends CRUD<Long, Payment> {
-}
