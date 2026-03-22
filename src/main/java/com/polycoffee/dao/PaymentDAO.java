94c9eacf963083c54b9e58351a1e93d9627ad06a
Nguyễn Nam <soniclolgaming@gmail.com>
2026-03-22 22:23:22 +0700

    Refactor: Implement Generic CRUD interface for DAOs

--- src/main/java/com/polycoffee/dao/PaymentDAO.java
+++ src/main/java/com/polycoffee/dao/PaymentDAO.java
@@ -1,12 +1,6 @@
 package com.polycoffee.dao;
 
 import com.polycoffee.entity.Payment;
-import java.util.List;
 
-public interface PaymentDAO {
-    void insert(Payment entity);
-    void update(Payment entity);
-    void delete(Long id);
-    Payment findById(Long id);
-    List<Payment> findAll();
+public interface PaymentDAO extends CRUD<Long, Payment> {
 }
