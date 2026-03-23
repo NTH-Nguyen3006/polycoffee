7570cabe6f81923070f8b5a88b4cb994f1befdbd
NTH-Nguyen <nthn300607@gmail.com>
2026-03-23 10:08:28 +0700

    update sql and entity

--- src/main/java/com/polycoffee/entity/Categories.java
+++ src/main/java/com/polycoffee/entity/Categories.java
@@ -2,10 +2,13 @@ package com.polycoffee.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -14,9 +17,11 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Categories {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
 
     @Column(columnDefinition = "NVARCHAR(50)")
--- src/main/java/com/polycoffee/entity/Users.java
+++ src/main/java/com/polycoffee/entity/Users.java
@@ -25,16 +25,19 @@ public class Users {
     @Id
     String id;
 
+    @Column(columnDefinition = "NVARCHAR(20)", unique = true)
+    String username;
+
     @Column(columnDefinition = "NVARCHAR(50)")
     String fullname;
 
-    @Column(columnDefinition = "VARCHAR(25)")
+    @Column(columnDefinition = "VARCHAR(25)", unique = true)
     String email;
 
     String password;
 
-    @Column(columnDefinition = "VARCHAR(10)")
-    String phone;
+    @Column(columnDefinition = "VARCHAR(9)")
+    String phone; // khử số 0
 
     UserRole role;
 
