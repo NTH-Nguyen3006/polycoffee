0f93a410f4b00d08f0a7c9be71b88377dae3edc8
NTH-Nguyen <nthn300607@gmail.com>
2026-03-23 11:21:47 +0700

    update DAO

--- src/main/java/com/polycoffee/entity/Users.java
+++ src/main/java/com/polycoffee/entity/Users.java
@@ -12,10 +12,12 @@ import com.polycoffee.enums.UserRole;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+@Builder
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
